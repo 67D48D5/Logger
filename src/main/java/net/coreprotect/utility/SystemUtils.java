3b059bde58008cc751b4c138e7ff3f9d5c9f70b4
67D48D5 <kernel@yonsei.ac.kr>
2026-01-18 12:53:43 +0900

    Support `Termux` environment by removing `oshi` dependency

--- src/main/java/net/coreprotect/utility/SystemUtils.java
+++ src/main/java/net/coreprotect/utility/SystemUtils.java
@@ -9,9 +9,6 @@ import java.util.regex.Pattern;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.core.config.Configurator;
 
-import oshi.SystemInfo;
-import oshi.hardware.CentralProcessor;
-
 public class SystemUtils {
 
     private static boolean testMode = Boolean.getBoolean("net.coreprotect.test");
@@ -211,42 +208,6 @@ public class SystemUtils {
         return null;
     }
 
-    public static CentralProcessor getProcessorInfo() {
-        // In test mode, don't actually try to initialize hardware components
-        if (testMode || isLog4jDisabled()) {
-            return null;
-        }
-
-        CentralProcessor result = null;
-        try {
-            Class.forName("com.sun.jna.Platform");
-            if (System.getProperty("os.name").startsWith("Windows") && !System.getProperty("sun.arch.data.model").equals("64")) {
-                Class.forName("com.sun.jna.platform.win32.Win32Exception");
-            }
-            else if (System.getProperty("os.name").toLowerCase().contains("android") || System.getProperty("java.runtime.name").toLowerCase().contains("android")) {
-                return null;
-            }
-
-            try {
-                if (!log4jInitialized) {
-                    Configurator.setLevel("oshi.hardware.common.AbstractCentralProcessor", Level.OFF);
-                    log4jInitialized = true;
-                }
-            }
-            catch (Exception e) {
-                // log4j configuration failure, continue without it
-            }
-
-            SystemInfo systemInfo = new SystemInfo();
-            result = systemInfo.getHardware().getProcessor();
-        }
-        catch (Exception e) {
-            // unable to read processor information
-        }
-
-        return result;
-    }
-
     /**
      * Get processor information string (for testing)
      * 
@@ -254,12 +215,6 @@ public class SystemUtils {
      */
     public static String getProcessorInfoString() {
         if (processorInfo != null) {
-            return processorInfo;
-        }
-
-        CentralProcessor processor = getProcessorInfo();
-        if (processor != null) {
-            processorInfo = processor.getProcessorIdentifier().getName();
             return processorInfo;
         }
 
